d9a59dbd5812cf5201f201800cddd9494d4ff5ed
Martynas Kadiša <8273932+martynaskadisa@users.noreply.github.com>
2019-03-19 22:10:39 +0200

    loosen up typings

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,28 +1,23 @@
-import { ActionCreator, Action } from './actions';
-import { AnyAction } from 'redux';
+import { AnyAction } from 'redux'
 
-export type Reducer<TState, TAction extends AnyAction> = (state: TState, action: TAction) => TState;
+export type Reducer<TState, TAction extends AnyAction> = (state: TState, action: TAction) => TState
 
-export const reduce = <TState, TPayload, TMeta>(
-    actionCreator: ActionCreator<TPayload, TMeta>,
-    reducer: Reducer<TState, Action<TPayload, TMeta>>
-): Record<
-    string, 
-    Reducer<TState, Action<TPayload, TMeta>>
-> => ({ [actionCreator.toString()]: reducer })
+export const reduce = <TState, TAction extends AnyAction>(
+  actionCreator: (...args: any[]) => TAction,
+  reducer: Reducer<TState, TAction>
+): Record<string, Reducer<TState, TAction>> => ({ [actionCreator.toString()]: reducer })
 
 export const createReducer = <TState>(
-    defaultState: TState, 
-    ...reducers: Array<Record<string, Reducer<TState, any>>>
+  defaultState: TState,
+  ...reducers: Array<Record<string, Reducer<TState, any>>>
 ) => {
-    const reducersMap = reducers.reduce((prev, next) => ({ ...prev, ...next }), {});
+  const reducersMap = reducers.reduce((prev, next) => ({ ...prev, ...next }), {})
 
-    return (state: TState = defaultState, action: AnyAction): TState => {
-      if (reducersMap.hasOwnProperty(action.type)) {
-        return reducersMap[action.type](state, action);
-      }
-
-      return state;
+  return (state: TState = defaultState, action: AnyAction): TState => {
+    if (reducersMap.hasOwnProperty(action.type)) {
+      return reducersMap[action.type](state, action)
     }
-}
 
+    return state
+  }
+}
